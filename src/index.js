e8b45215de242719620ee914eed61ccd005a0a50
Errorcrafter <xlmodder180@gmail.com>
2022-03-24 20:50:28 +0800

    ive updating colour pickers and ability fields

--- src/index.js
+++ src/index.js
@@ -28,31 +28,57 @@ function startup() {
 
     var raritySelector = document.querySelector("select#setRarity");
 
+    var abil1NameColour = document.querySelector("input#abil1NameColour");
+    var abil1NameColourHex = document.querySelector("input#abil1NameColourHex");
+    var abil2NameColour = document.querySelector("input#abil2NameColour");
+    var abil2NameColourHex = document.querySelector("input#abil2NameColourHex");
+    var enableAbil2 = document.querySelector("input#enableAbil2");
+
     // Init defaults
     itemNameColour.value = "#ffffff";
     itemNameColourHex.value = "#ffffff";
     leatherColour.value = "#ffffff";
     leatherColourHex.value = "#ffffff";
+    abil1NameColour.value = "#ffffff";
+    abil1NameColourHex.value = "#ffffff";
+    abil2NameColour.value = "#ffffff";
+    abil2NameColourHex.value = "#ffffff";
 
     baseSetDropdown.value = "iron";
+    raritySelector.value = "common";
     leatherCustomColourToggle.checked = false;
+    document.querySelector("select#abil1Type").value = "click";
+    document.querySelector("select#abil2Type").value = "click";
+    enableAbil2.checked = false;
+    document.querySelector("textarea#abil1Description").value = "";
+    document.querySelector("textarea#abil2Description").value = "";
+    
     leatherCustomColourToggleSection.style = "display: none;";
     leatherItemColourPicker.style = "display: none;";
-
-    raritySelector.value = "common";
+    document.querySelector("tbody#abil2Section").style = "display: none;";
 
     // Event listeners for syncing colour inputs with hex code inputs
-    itemNameColour.addEventListener("change", function () { itemNameColourHex.value = itemNameColour.value; }, false);
-    itemNameColourHex.addEventListener("change", function () { itemNameColour.value = itemNameColourHex.value; }, false);
+    itemNameColour.addEventListener("input", function () { itemNameColourHex.value = itemNameColour.value; }, false);
+    itemNameColourHex.addEventListener("input", function () { itemNameColour.value = itemNameColourHex.value; }, false);
 
-    baseSetDropdown.addEventListener("change", function () {
+    baseSetDropdown.addEventListener("input", function () {
         baseSetDropdown.value === "leather" ? leatherCustomColourToggleSection.style = "" : leatherCustomColourToggleSection.style = "display: none;"; leatherItemColourPicker.style = "display: none;"
     });
-    leatherCustomColourToggle.addEventListener("change", function () {
+    leatherCustomColourToggle.addEventListener("input", function () {
         leatherCustomColourToggle.checked ? leatherItemColourPicker.style = "" : leatherItemColourPicker.style = "display: none;"
     });
-    leatherColour.addEventListener("change", function () { leatherColourHex.value = leatherColour.value; }, false);
-    leatherColourHex.addEventListener("change", function () { leatherColour.value = leatherColourHex.value; }, false);
+    leatherColour.addEventListener("input", function () { leatherColourHex.value = leatherColour.value; }, false);
+    leatherColourHex.addEventListener("input", function () { leatherColour.value = leatherColourHex.value; }, false);
+
+    abil1NameColour.addEventListener("input", function () { abil1NameColourHex.value = abil1NameColour.value; }, false);
+    abil1NameColourHex.addEventListener("input", function () { abil1NameColour.value = abil1NameColourHex.value; }, false);
+
+    enableAbil2.addEventListener("input", function () {
+        console.log("a "+enableAbil2.checked)
+        enableAbil2.checked ? document.querySelector("tbody#abil2Section").style = "" : document.querySelector("tbody#abil2Section").style = "display: none;"
+    });
+    abil2NameColour.addEventListener("input", function () { abil2NameColourHex.value = abil2NameColour.value; }, false);
+    abil2NameColourHex.addEventListener("input", function () { abil2NameColour.value = abil2NameColourHex.value; }, false);
 }
 
 function appendStatSelectors(parent, names, mode) {
